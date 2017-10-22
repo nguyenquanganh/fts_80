9cb9105dea57983cb50e2b8363f5d6381a8acdac
taylqse04184 <taylqse04184@fpt.edu.vn>
2017-10-22 12:59:33 +0700

    fix comment a sang

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,8 +4,19 @@ class Test < ApplicationRecord
 
   has_many :results, dependent: :destroy
   has_many :questions, ->{distinct}, through: :results, dependent: :destroy
+  has_many :answers, ->{distinct}, through: :results, dependent: :destroy
+  has_many :question_answers, ->{distinct}, through: :questions,
+    dependent: :destroy, source: :answers, class_name: Answer.name
 
   def answer_ids_of_question question_id
     results.where(question_id: question_id).pluck :answer_id
+  end
+
+  def correct_answer_of_user
+    answers.where(is_correct: true).count
+  end
+
+  def correct_answer_of_test
+    question_answers.where(is_correct: true).count
   end
 end
